5158952c878d0db8d0c2d1cf91e809a9220eb6e0
AswinCastro <aswin.castro@qruize.com>
2017-06-26 12:33:31 +0530

    1st commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { AddComponent } from './customer/add/add.component';
+import { EditComponent } from './customer/edit/edit.component';
+import { DetailComponent } from './customer/detail/detail.component';
+import {ChartModule} from 'primeng/primeng';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    AddComponent,
+    EditComponent,
+    DetailComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ChartModule
   ],
   providers: [],
   bootstrap: [AppComponent]
